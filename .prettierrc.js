f7cecb944c6a18a1819f5c3fac15234057f656c5
proxime <kulaknorbert@gmail.com>
2024-02-07 19:39:10 +0100

    feat: initialize project

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  tabWidth: 4,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSameLine: false,
+  endOfLine: "lf",
+  printWidth: 100,
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.ts"
+}
